e32c881835f2d7ce7ceff5b81d3d68b9608e6ad9
mikelee2004 <strober2004@gmail.com>
2023-09-06 21:08:14 +0300

    rock, ppr, scissors

---
+++ rockpprczrs.py
@@ -0,0 +1,22 @@
+player1 = input('Камень, ножницы, бумага? ')
+player2 = input('Камень, ножницы, бумага? ')
+
+player1 = player1.lower()
+player2 = player2.lower()
+
+if player1 == 'камень' and player2 == 'ножницы':
+    print('Игрок 1 победил!')
+elif player1 == 'ножницы' and player2 == 'бумага':
+    print('Игрок 1 победил!')
+elif player1 == 'бумага' and player2 == 'камень':
+    print('Игрок 1 победил!')
+elif player2 == 'камень' and player1 == 'ножницы':
+    print('Игрок 2 победил!')
+elif player2 == 'ножницы' and player1 == 'бумага':
+    print('Игрок 2 победил!')
+elif player2 == 'бумага' and player1 == 'камень':
+    print('Игрок 2 победил!')
+elif player1==player2:
+    print('Ничья!')
+else:
+    print('Кто-то ошибся!')
