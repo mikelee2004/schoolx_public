c22e5027ea9cf03a0069b6324f4bd50b6023755d
mikelee2004 <strober2004@gmail.com>
2023-09-08 11:44:36 +0300

    ya slowpok

--- practice3/simple_guess.py
+++ practice3/simple_guess.py
@@ -6,9 +6,8 @@ def guess(num: int) -> int | str:
     for i in range(1, num+1):
         if i**2 == num:
             return i
-        else:
-            i = i + 1
-
+        if i not in range(1, num+1):
+            return 'ya tak ne mogu bolshe'
 num = int(input('Введи целое натуральное число: '))
 
 print(guess(num))
